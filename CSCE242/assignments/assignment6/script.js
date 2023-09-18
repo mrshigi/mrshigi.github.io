e8b01f65bcb57aa741dce6be03b064976ef49dba
mrshigi <sraudat@email.sc.edu>
2023-09-18 03:52:22 -0400

    changes to input to almost have it perfect

--- CSCE242/assignments/assignment6/script.js
+++ CSCE242/assignments/assignment6/script.js
@@ -20,10 +20,10 @@ const displayComment = () =>
     const comment = document.getElementById("txt-comment").value;
     const rating = document.getElementById("txt-rating").value;
     const name = document.getElementById("txt-user-name").value;
-    const h1 = document.createElement("p");
+    const usercomment = document.createElement("tr");
     const textNode = document.createTextNode("Product: "+ product+"Comment: "+ comment+ "Rating: "+ rating + "User Name: " + name);
-    h1.appendChild(textNode);
-    document.getElementById("comment").appendChild(h1);
+    usercomment.appendChild(textNode);
+    document.getElementById("comment").appendChild(usercomment);
 }
 
 window.onload = () =>
