3855ea1a7235be0942098495cb79d33bbdc41873
mrshigi <sraudat@email.sc.edu>
2023-09-18 01:05:33 -0400

    assignment 6 stuff

--- CSCE242/assignments/assignment6/script.js
+++ CSCE242/assignments/assignment6/script.js
@@ -1,17 +1,35 @@
+const showImage = () => 
+{
+    document.getElementById('image').style.display='block';
+}
+const hideImage = () => 
+{
+    document.getElementById('image').style.display='none'
+}
+
+
 
 const moveSquare = () => 
 {
     document.getElementById("emoji").classList.add("move-square");
 }
 
-const displayName = () => 
+const displayComment = () => 
 {
-    const firstName = document.getElementById("txt-first-name").value;
-    console.log("Hello " + firstName + "!");
+    const product = document.getElementById("txt-product-name").value;
+    const comment = document.getElementById("txt-comment").value;
+    const rating = document.getElementById("txt-rating").value;
+    const name = document.getElementById("txt-user-name").value;
+    const h1 = document.createElement("H1");
+    const textNode = document.createTextNode("Product");
+    h1.appendChild(textNode);
+    document.body.appendChild(h1);  
 }
 
-window.onload = () => 
+window.onload = () =>
 {
+    document.getElementById("button-show").onclick = showImage;
+    document.getElementById("button-hide").onclick = hideImage;
+    document.getElementById("button-comment").onclick = displayComment;
     document.getElementById("button-move").onclick = moveSquare;
-    document.getElementById("button-show-name").onclick = displayName;
 }
