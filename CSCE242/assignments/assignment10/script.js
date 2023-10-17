fca6f545713e47e0011ad8cbe9beac76859d2a6e
mrshigi <sraudat@email.sc.edu>
2023-10-17 15:16:16 -0400

    a10 perfected

--- CSCE242/assignments/assignment10/script.js
+++ CSCE242/assignments/assignment10/script.js
@@ -1,65 +1,71 @@
 class Toy {
-  constructor(imageFileName, name) {
-      this.imageFileName = imageFileName;
+    constructor(imageFileName, name, description, price) {
       this.name = name;
-  }
+      this.imageFileName = imageFileName;
+      this.description = description;
+      this.price = price;
+    }
 
-  get details() {
-      return `${this.name}`;
-  }
+    getDetails() {
+      return `${this.name}: ${this.description} - $${this.price}`;
+    }
 
-  getToyItem() {
+    getToyItem() {
       return {
-          imageFileName: this.imageFileName,
-          name: this.name
+        name: this.name,
+        imageFileName: this.imageFileName,
+        description: this.description,
+        price: this.price,
       };
+    }
   }
-}
 
-const toys = [
-  new Toy("toy1.png", "Godzilla toy"),
-  new Toy("toy2.png", "Sword"),
-  new Toy("toy3.png", "Sword of my Childhood"),
-  new Toy("toy4.png", "The one ring"),
-  new Toy("toy5.png", "Nerf Ball"),
-  new Toy("toy6.png", "Nerf Gun")
-];
+  const toys = [
+    new Toy("toy1.png", "Godzilla toy", "Age - 3-5", 17.99),
+    new Toy("toy2.png", "Sword", "Age - 6-75", 1699.99),
+    new Toy("toy3.png", "Sword of my Childhood", "Age - 3-8", 0),
+    new Toy("toy4.png", "The One Ring", "The One True Ring, a Soul must be given", 1000000000000),
+    new Toy("toy5.png", "Nerf Ball", "Age - 3-21", 4.20),
+    new Toy("toy6.png", "Nerf Gun", "Age - 6-8", 4.20),
+  ];
 
-function createToyElement(toy) {
-  const container = document.createElement("div");
-  container.classList.add("flex-container");
+  function createToyElement(toy) {
+    const container = document.createElement("div");
+    container.classList.add("flex-container");
 
-  const innerContainer = document.createElement("div");
-  innerContainer.classList.add("container");
+    const innerContainer = document.createElement("div");
+    innerContainer.classList.add("container");
 
-  const image = document.createElement("img");
-  image.src = `images/toy.${toy.imageFileName}`;
-  image.alt = "Toy Image";
-  image.style.width = "100%";
+    const image = document.createElement("img");
+    image.src = `images/${toy.imageFileName}`;
+    image.alt = "Toy Image";
+    image.style.width = "200px";
 
-  const middle = document.createElement("div");
-  middle.classList.add("middle");
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("tooltip");
+    tooltip.innerText = 
+    `${toy.name}
+    ${toy.description}
+    $${toy.price}`;
 
-  const text = document.createElement("div");
-  text.classList.add("text");
-  text.innerText = toy.name;
+    image.addEventListener("mouseover", () => {
+      tooltip.style.visibility = "visible";
+    });
 
-  middle.appendChild(text);
-  innerContainer.appendChild(image);
-  innerContainer.appendChild(middle);
-  container.appendChild(innerContainer);
+    innerContainer.appendChild(image);
+    innerContainer.appendChild(tooltip);
+    container.appendChild(innerContainer);
 
-  return container;
-}
+    return container;
+  }
 
-function displayToys() {
-  const toyContainer = document.getElementById("toy-container");
+  function displayToys() {
+    const toyContainer = document.getElementById("toy-container");
 
-  toys.forEach(toy => {
+    toys.forEach(toy => {
       const toyElement = createToyElement(toy);
       toyContainer.appendChild(toyElement);
-  });
-}
+    });
+  }
 
-// Call the function to display the toys
-displayToys();
+  displayToys();
